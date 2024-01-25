06b4483d5f2576405d6c9edb9303a65d21770d9a
joaomluis <jooluis2@gmail.com>
2024-01-25 09:37:00 +0000

    added: create task

--- main-page-scripts.js
+++ main-page-scripts.js
@@ -43,3 +43,80 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
 });
+
+
+//função para fazer get do username da storage e atribuir esse username à label da pagina incial
+document.addEventListener('DOMContentLoaded', function() {
+    const username = sessionStorage.getItem('username');
+
+    const welcomeLabel = document.getElementById('welcome-user');
+    if (welcomeLabel) {
+        welcomeLabel.textContent = 'Welcome, ' + username;
+    }
+});
+
+function logout() {
+    window.location.href='index.html';
+}
+
+function openModal() {
+    var modal = document.getElementById("taskModal");
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    var modal = document.getElementById("taskModal");
+    modal.style.display = "none";
+}
+
+function createTask() {
+    var taskName = document.getElementById("taskName").value;
+    var taskDescription = document.getElementById("text-area").value;
+
+    if (taskName) {
+        var newTask = document.createElement("div");
+        newTask.className = "task";
+        newTask.draggable = true;
+        newTask.innerHTML = `<p>${taskName}</p><p>${taskDescription}</p>`;
+
+        var toDoList = document.getElementById("to-do-list");
+        toDoList.appendChild(newTask);
+
+        closeModal();
+    }
+}
+
+//funçoes para adicionar tasks as boxes que o prof fez 
+
+function fromHTML(html, trim = true) {
+    // Process the HTML string.
+    html = trim ? html : html.trim();
+    if (!html) return null;
+  
+    // Then set up a new template element.
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    const result = template.content.children;
+  
+    // Then return either an HTMLElement or HTMLCollection,
+    // based on whether the input HTML had one or more roots.
+    if (result.length === 1) return result[0];
+    return result;
+  }
+
+function addDummyData() {
+    const tasks = [
+        {title: "teste1", description: "testedesc"},
+        {title: "teste2", description: "testedesc"},
+        {title: "teste3", description: "testedesc"},
+    ];
+
+    const todoDiv = document.querySelector('#box1');
+    for (const task of tasks) {
+        console.log(task);
+        const newTask = fromHTML(`<div class="task" draggable="true">${task.title}</div>`);
+        todoDiv.append(newTask);
+
+    }
+}
+
--- scripts.js
+++ scripts.js
@@ -3,9 +3,12 @@ function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
+    
+
     if (username === '' || password === '') {
         alert('Please enter both username and password')
     } else {
+            sessionStorage.setItem('username', username);
             window.location.href = 'main-page.html';  
         };
     }
