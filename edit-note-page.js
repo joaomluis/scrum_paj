c43bd2437a4a4da5da7d0fdb7fa88be1fa3077ae
Joao Morais <joaomorais@student.dei.uc.pt>
2024-01-26 09:55:24 +0000

    Auto stash before merge of "main" and "origin/main"

---
+++ edit-note-page.js
@@ -0,0 +1,9 @@
+document.getElementById("butao").onclick = function(){
+    const updatedTitle = document.getElementById('titlename').value;
+    const updatedStage = document.getElementById('stage').value;
+    const updatedDescription = document.getElementById('description').value;
+    console.log('Updated Title:', updatedTitle);
+    console.log('Updated Stage:', updatedStage);
+    console.log('Updated Description:', updatedDescription);
+}
+
